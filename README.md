538962535c0fb3b714c79763c933f2eb774297c3
Mark Page <mark.page@burdenandburden.co.uk>
2021-02-03 17:36:19 +0000

    Adds rebrand.sh

--- README.md
+++ README.md
@@ -3,4 +3,10 @@
 
 Default branding that may be copied to a new project create a brand built on blotto core software
 
+You can take this code and use it to brand blotto2. Typically like this:
+
+git clone https://github.com/whitelamp-uk/blotto.git
+git clone https://github.com/whitelamp-uk/blotto-brand.git
+sh blotto-brand/rebrand.sh my-brand-name
+ls -l
 
---
+++ rebrand.sh
@@ -0,0 +1,25 @@
+
+if [ ! "$1" ]
+then
+    echo "First argument is, for example, your-brand-name"
+    exit
+fi
+
+# Where are we?
+wd="$(pwd)"
+
+# Descend into project
+cd "$(dirname $0)"
+rm -rf .git
+rm LICENSE
+mv blotto-brand.sublime-project blotto-$1.sublime-project
+
+# Ascend into parent directory
+cd ..
+mv blotto-brand blotto-$1
+ln -s blotto-$1 blotto-brand
+
+# Return to where we started
+cd "$wd"
+
+
