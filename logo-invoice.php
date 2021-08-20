213e1ab423a62ffa92ad7ae75bc5167e3d487a93
Mark Page <mark.page@burdenandburden.co.uk>
2021-08-20 10:41:24 +0000

    Adds HTML snippet for invoice embedded logo

---
+++ logo-invoice.php
@@ -0,0 +1,7 @@
+<!-- TODO: the SVG embed is not right ->>
+<img
+  title="Whitelamp"
+  alt="&quot;whitelamp&quot; in white overlaid on a spectrum of colours from blue through red to peach"
+  src="<?php require __DIR__.'/logo-invoice.svg' ?>"
+/>
+
