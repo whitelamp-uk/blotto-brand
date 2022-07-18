ba043c9544181415534973ceac5997adbbf33521
Mark Page <mark.page@burdenandburden.co.uk>
2022-07-18 09:49:14 +0000

    Adds contemporary prize information

--- about.php
+++ about.php
@@ -1,3 +1,11 @@
+<?php
+$sat_next = new \DateTime ('2022-07-23');
+$sat_next->add (new DateInterval('P1D'));
+while ($sat_next->format('w')!=6) {
+    $sat_next->add (new DateInterval('P1D'));
+}
+$sat_next = $sat_next->format ('Y-m-d');
+?>
 
     <section id="about" class="content">
 
@@ -21,6 +29,66 @@
               <td>Proof</td>
               <td><a target="_blank" href="./proof/">Browse</a></td>
             </tr>
+            <tr>
+              <td colspan="2"><strong>Prizes @ <?php echo gmdate ('Y-m-d'); ?></strong></td>
+            </tr>
+<?php foreach (prizes(gmdate('Y-m-d')) as $p): ?>
+            <tr>
+              <td>Prize <?php echo intval($p['level']); ?></td>
+              <td>
+                <?php echo htmlspecialchars ($p['name']); ?>
+<?php     if ($p['insure']): ?>
+                (insured)
+<?php     endif; ?>
+<?php     if ($p['quantity']): ?>
+                <br/>
+                Quantity: <?php echo htmlspecialchars ($p['quantity']); ?>
+<?php     endif; ?>
+<?php     if ($p['quantity_percent']): ?>
+                <br/>
+                Quantity: <?php echo htmlspecialchars ($p['quantity_percent']); ?>%
+<?php     endif; ?>
+<?php     if ($p['amount']): ?>
+                <br/>
+                Monetary value: <?php echo htmlspecialchars (BLOTTO_CURRENCY.$p['amount']); ?>
+<?php     endif; ?>
+<?php     if ($p['function_name']): ?>
+                <br/>
+                Function: <?php echo htmlspecialchars ($p['function_name']); ?>()
+<?php     endif; ?>
+              </td>
+            </tr>
+<?php endforeach; ?>
+            <tr>
+              <td colspan="2"><strong>Prizes @ <?php echo $sat_next; ?></strong></td>
+            </tr>
+<?php foreach (prizes($sat_next) as $p): ?>
+            <tr>
+              <td>Prize <?php echo intval($p['level']); ?></td>
+              <td>
+                <?php echo htmlspecialchars ($p['name']); ?>
+<?php     if ($p['insure']): ?>
+                (insured)
+<?php     endif; ?>
+<?php     if ($p['quantity']): ?>
+                <br/>
+                Quantity: <?php echo htmlspecialchars ($p['quantity']); ?>
+<?php     endif; ?>
+<?php     if ($p['quantity_percent']): ?>
+                <br/>
+                Quantity: <?php echo htmlspecialchars ($p['quantity_percent']); ?>%
+<?php     endif; ?>
+<?php     if ($p['amount']): ?>
+                <br/>
+                Monetary value: <?php echo htmlspecialchars (BLOTTO_CURRENCY.$p['amount']); ?>
+<?php     endif; ?>
+<?php     if ($p['function_name']): ?>
+                <br/>
+                Function: <?php echo htmlspecialchars ($p['function_name']); ?>()
+<?php     endif; ?>
+              </td>
+            </tr>
+<?php endforeach; ?>
             <tr>
               <td>Our address</td>
               <td><?php require __DIR__.'/address.php'; ?></td>
