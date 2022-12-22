c9ab35d6ae3130deae198338c9edda11f1de6a4e
Mark Page <mark.page@burdenandburden.co.uk>
2022-12-22 13:47:30 +0000

    Improves prize look-ahead reporting

--- about.php
+++ about.php
@@ -1,10 +1,13 @@
 <?php
-$sat_next = new \DateTime ();
-$sat_next->add (new DateInterval('P1D'));
-while ($sat_next->format('w')!=6) {
-    $sat_next->add (new DateInterval('P1D'));
+// Prizes for next four draws
+$draws          = [];
+$dt             = new \DateTime ();
+for ($i=0;$i<5;$i++) {
+    $dc         = draw_upcoming ($dt->format ('Y-m-d'));
+    $draws[]    = draw ($dc);
+    $dt         = new \DateTime ($dc);
+    $dt->add (new DateInterval('P1D'));
 }
-$sat_next = $sat_next->format ('Y-m-d');
 ?>
 
     <section id="about" class="content">
@@ -29,65 +32,37 @@ $sat_next = $sat_next->format ('Y-m-d');
               <td>Proof</td>
               <td><a target="_blank" href="./proof/">Browse</a></td>
             </tr>
+<?php foreach ($draws as $d): ?>
             <tr>
-              <td colspan="2"><strong>Prizes @ <?php echo gmdate ('Y-m-d'); ?></strong></td>
+              <td colspan="2"><strong>Prizes @ <?php echo $d->date; ?></strong></td>
             </tr>
-<?php foreach (prizes(gmdate('Y-m-d')) as $p): ?>
-            <tr>
-              <td>Prize <?php echo intval($p['level']); ?></td>
-              <td>
-                <?php echo htmlspecialchars ($p['name']); ?>
-<?php     if ($p['insure']): ?>
-                (insured)
-<?php     endif; ?>
-<?php     if ($p['quantity']): ?>
-                <br/>
-                Quantity: <?php echo htmlspecialchars ($p['quantity']); ?>
-<?php     endif; ?>
-<?php     if ($p['quantity_percent']): ?>
-                <br/>
-                Quantity: <?php echo htmlspecialchars ($p['quantity_percent']); ?>%
-<?php     endif; ?>
-<?php     if ($p['amount']): ?>
-                <br/>
-                Monetary value: <?php echo htmlspecialchars (BLOTTO_CURRENCY.$p['amount']); ?>
-<?php     endif; ?>
-<?php     if ($p['function_name']): ?>
-                <br/>
-                Function: <?php echo htmlspecialchars ($p['function_name']); ?>()
-<?php     endif; ?>
-              </td>
-            </tr>
-<?php endforeach; ?>
-            <tr>
-              <td colspan="2"><strong>Prizes @ <?php echo $sat_next; ?></strong></td>
-            </tr>
-<?php foreach (prizes($sat_next) as $p): ?>
+<?php     foreach ($d->prizes as $p): ?>
             <tr>
               <td>Prize <?php echo intval($p['level']); ?></td>
               <td>
                 <?php echo htmlspecialchars ($p['name']); ?>
-<?php     if ($p['insure']): ?>
+<?php         if ($p['insure']): ?>
                 (insured)
-<?php     endif; ?>
-<?php     if ($p['quantity']): ?>
+<?php         endif; ?>
+<?php         if ($p['quantity']): ?>
                 <br/>
                 Quantity: <?php echo htmlspecialchars ($p['quantity']); ?>
-<?php     endif; ?>
-<?php     if ($p['quantity_percent']): ?>
+<?php         endif; ?>
+<?php         if ($p['quantity_percent']): ?>
                 <br/>
                 Quantity: <?php echo htmlspecialchars ($p['quantity_percent']); ?>%
-<?php     endif; ?>
-<?php     if ($p['amount']): ?>
+<?php         endif; ?>
+<?php         if ($p['amount']): ?>
                 <br/>
                 Monetary value: <?php echo htmlspecialchars (BLOTTO_CURRENCY.$p['amount']); ?>
-<?php     endif; ?>
-<?php     if ($p['function_name']): ?>
+<?php         endif; ?>
+<?php         if ($p['function_name']): ?>
                 <br/>
                 Function: <?php echo htmlspecialchars ($p['function_name']); ?>()
-<?php     endif; ?>
+<?php         endif; ?>
               </td>
             </tr>
+<?php     endforeach; ?>
 <?php endforeach; ?>
             <tr>
               <td>Our address</td>
