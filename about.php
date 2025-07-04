6f37e5c11a775d7cd4f03e41bc789974d14b844f
Mark Page <mark.page@burdenandburden.co.uk>
2025-07-04 14:46:22 +0000

    Revises About view technical details

--- about.php
+++ about.php
@@ -94,98 +94,128 @@ document.addEventListener (
 );
       </script>
 
-      <table class="report">
-        <tbody>
-          <tr>
-            <td colspan="2"><h2>Technical details</h2></td>
-          </tr>
-          <tr>
-            <td colspan="2"><h3>Required by you</h3></td>
-          </tr>
-          <tr>
-            <td colspan="2"><strong>Integration details</strong></td>
-          </tr>
-          <tr>
-            <td>Web APIs</td>
-            <td><a target="_blank" href="./demo.php">View demo source code</a></td>
-          </tr>
-          <tr>
-            <td>Self-service sign-up (direct debit) URL</td>
-            <td><a class="write-url" target="_blank" href="./sss.php">sss.php</a></td>
-          </tr>
-          <tr>
-            <td>Online tickets (ad hoc) URL</td>
-            <td><a class="write-url" target="_blank" href="./tickets.php">tickets.php</a></td>
-          </tr>
-          <tr>
-            <td>Online tickets (promotional draw) URL</td>
-            <td><a class="write-url" target="_blank" href="./tickets.php?d=2022-04-07">tickets.php?d=2022-04-07</a></td>
-          </tr>
-<?php foreach (www_pay_apis() as $code=>$api): ?>
-          <tr>
-            <td>One-off online tickets payment provider callback URL (<?php echo htmlspecialchars ($code); ?>)</td>
-            <td><a class="write-url" href="./callback.php?p=<?php echo htmlspecialchars ($code); ?>">callback.php?p=<?php echo htmlspecialchars ($code); ?></a></td>
-          </tr>
-<?php endforeach; ?>
-          <tr>
-            <td colspan="2"><h3>Provided by you</h3></td>
-          </tr>
-<?php $org = org (); ?>
-          <tr>
-            <td colspan="2"><strong>Self-service sign-up</strong></td>
-          </tr>
-          <tr>
-            <td>SUN (direct debit service user number)</td>
-            <td><?php if (defined('BLOTTO_SSS_SUN') && BLOTTO_SSS_SUN) {echo htmlspecialchars (BLOTTO_SSS_SUN);} else { echo '[Please report to administrator]'; } ?></td>
-          </tr>
-          <tr>
-            <td colspan="2"><strong>General</strong></td>
-          </tr>
-          <tr>
-            <td>Terms &amp; conditions URL</td>
-            <td><?php if ($org['signup_url_terms']) {echo htmlspecialchars ($org['signup_url_terms']);} else { echo '[Please report to administrator]'; } ?></td>
-          </tr>
-          <tr>
-            <td>Privacy statement URL</td>
-            <td><?php if ($org['signup_url_privacy']) {echo htmlspecialchars ($org['signup_url_privacy']);} else { echo '[Please report to administrator]'; } ?></td>
-          </tr>
-          <tr>
-            <td>Supporter contact email</td>
-            <td><?php if ($org['admin_email']) {echo htmlspecialchars ($org['admin_email']);} else { echo '[Please report to administrator]'; } ?></td>
-          </tr>
-          <tr>
-            <td>Supporter contact telephone</td>
-            <td><?php if ($org['admin_phone']) {echo htmlspecialchars ($org['admin_phone']);} else { echo '[Please report to administrator]'; } ?></td>
-          </tr>
-          <tr>
-            <td>Self-exclusion email address</td>
-            <td><?php if (defined('BLOTTO_SSS_SELFEX_EMAIL') && BLOTTO_SSS_SELFEX_EMAIL) {echo htmlspecialchars (BLOTTO_SSS_SELFEX_EMAIL);} else { echo '[Please report to administrator]'; } ?></td>
-          </tr>
+      <form method="post" action="">
 
-          <tr>
-            <td colspan="2"><h3>Other configuration</h3></td>
-          </tr>
-          <tr>
-            <td colspan="2"><strong>Fees @ <?php echo gmdate ('Y-m-d'); ?></strong></td>
-          </tr>
+        <table class="report">
+          <tbody>
+            <tr>
+              <td colspan="2"><h2>Technical details</h2></td>
+            </tr>
+            <tr>
+              <td colspan="2"><h3>Required by you</h3></td>
+            </tr>
+            <tr>
+              <td colspan="2"><strong>Integration details</strong></td>
+            </tr>
+            <tr>
+              <td>Web APIs</td>
+              <td><a target="_blank" href="./demo.php">View demo source code</a></td>
+            </tr>
+            <tr>
+              <td>Self-service sign-up (direct debit) URL</td>
+              <td><a class="write-url" target="_blank" href="./sss.php">sss.php</a></td>
+            </tr>
+            <tr>
+              <td>Online tickets (ad hoc) URL</td>
+              <td><a class="write-url" target="_blank" href="./tickets.php">tickets.php</a></td>
+            </tr>
+            <tr>
+              <td>Online tickets (promotional draw) URL</td>
+              <td><a class="write-url" target="_blank" href="./tickets.php?d=2022-04-07">tickets.php?d=2022-04-07</a></td>
+            </tr>
+<?php foreach (www_pay_apis() as $code=>$api): ?>
+            <tr>
+              <td>One-off online tickets payment provider callback URL (<?php echo htmlspecialchars ($code); ?>)</td>
+              <td><a class="write-url" href="./callback.php?p=<?php echo htmlspecialchars ($code); ?>">callback.php?p=<?php echo htmlspecialchars ($code); ?></a></td>
+            </tr>
+<?php endforeach; ?>
+            <tr>
+              <td colspan="2"><h3>Provided by you</h3></td>
+            </tr>
+<?php $org = org (); ?>
+            <tr>
+              <td colspan="2"><strong>Self-service sign-up</strong></td>
+            </tr>
+            <tr>
+              <td>SUN (direct debit service user number)</td>
+              <td><?php if (defined('BLOTTO_SSS_SUN') && BLOTTO_SSS_SUN) {echo htmlspecialchars (BLOTTO_SSS_SUN);} else { echo '[Please report to administrator]'; } ?></td>
+            </tr>
+            <tr>
+              <td colspan="2"><strong>General</strong></td>
+            </tr>
+            <tr>
+              <td>Terms &amp; conditions URL</td>
+              <td><?php if ($org['signup_url_terms']) {echo htmlspecialchars ($org['signup_url_terms']);} else { echo '[Please report to administrator]'; } ?></td>
+            </tr>
+            <tr>
+              <td>Privacy statement URL</td>
+              <td><?php if ($org['signup_url_privacy']) {echo htmlspecialchars ($org['signup_url_privacy']);} else { echo '[Please report to administrator]'; } ?></td>
+            </tr>
+            <tr>
+              <td>Supporter contact email</td>
+              <td><?php if ($org['admin_email']) {echo htmlspecialchars ($org['admin_email']);} else { echo '[Please report to administrator]'; } ?></td>
+            </tr>
+            <tr>
+              <td>Supporter contact telephone</td>
+              <td><?php if ($org['admin_phone']) {echo htmlspecialchars ($org['admin_phone']);} else { echo '[Please report to administrator]'; } ?></td>
+            </tr>
+            <tr>
+              <td>Self-exclusion email address</td>
+              <td><?php if (defined('BLOTTO_SSS_SELFEX_EMAIL') && BLOTTO_SSS_SELFEX_EMAIL) {echo htmlspecialchars (BLOTTO_SSS_SELFEX_EMAIL);} else { echo '[Please report to administrator]'; } ?></td>
+            </tr>  
+
+            <tr>
+              <td colspan="2"><h3>Other configuration</h3></td>
+            </tr>
+            <tr>
+              <td colspan="2"><strong>Fees @ <?php echo gmdate ('Y-m-d'); ?></strong></td>
+            </tr>
 <?php if (!count($fees=fees())): ?>
-          <tr>
-            <td>No fees configured</td>
-            <td><a target="_blank" href="./demo.php">View demo source code</a></td>
-          </tr>
+            <tr>
+              <td>No fees configured</td>
+              <td><a target="_blank" href="./demo.php">View demo source code</a></td>
+            </tr>
 <?php else: ?>
 <?php     foreach ($fees as $code=>$fee): ?>
 <?php         if ($code!='total'): ?>
-          <tr>
-            <td><code><?php echo htmlspecialchars ($code); ?></code><?php echo htmlspecialchars ($fee['notes']); ?></td>
-            <td><code><?php echo htmlspecialchars (BLOTTO_CURRENCY) . number_format ($fee['rate']/100,2,'.',''); ?></code></td>
-          </tr>
+            <tr>
+              <td><code><?php echo htmlspecialchars ($code); ?></code><?php echo htmlspecialchars ($fee['notes']); ?></td>
+              <td><code><?php echo htmlspecialchars (BLOTTO_CURRENCY) . number_format ($fee['rate']/100,2,'.',''); ?></code></td>
+            </tr>
 <?php         endif; ?>
 <?php     endforeach; ?>
 <?php endif; ?>
 
-        </tbody>
-      </table>
+            <tr>
+              <td colspan="2"><h3>Tests</h3></td>
+            </tr>
+            <tr>
+              <td colspan="2"><strong>ANL email templates</strong></td>
+            </tr>
+            <tr>
+              <td>Email address</td>
+              <td>
+                <input type="email" name="email" placeholder="Send test to this email" />
+              </td>
+            </tr>
+            <tr>
+              <td>Template ref</td>
+              <td>
+                <input type="text" name="template_ref" placeholder="Mailing service ID" />
+              </td>
+            </tr>
+            <tr>
+              <td>Template type</td>
+              <td>
+                <input type="submit" name="test_email_anl" value="Send random ANL" />
+                <input type="submit" name="test_email_win" value="Send random winner letter" />
+              </td>
+            </tr>
+
+          </tbody>
+        </table>
+
+    </form>
 
     </section>
 
