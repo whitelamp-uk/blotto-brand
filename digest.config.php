de4d1411c1b84cfc37448224b1a5ef90f0cb86fb
Mark Page <mark.page@burdenandburden.co.uk>
2025-08-06 10:32:15 +0000

    Adds config for global digest

---
+++ digest.config.php
@@ -0,0 +1,8 @@
+<?php
+
+define ( 'BLOTTO_DB', 'blotto_config' ); // DB user must have direct access to this database
+
+require '/home/blotto/config.global.php';
+require '/home/mark/blotto/blotto2/scripts/functions.php';
+
+
